477b158c336d1324ec974bfca87a1fa9acc019de
Vinicius Ghellere Sbardelotto <viniciusghellere@gmail.com>
2024-10-15 19:57:06 -0300

    test: TemperatureSample validators

--- test/models/temperature_sample_test.rb
+++ test/models/temperature_sample_test.rb
@@ -11,7 +11,33 @@
 require "test_helper"
 
 class TemperatureSampleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not create sample with an invalid 'value'" do
+    assert_raises ActiveRecord::RecordInvalid, match: "Validation failed: Value is not a number" do
+      TemperatureSample.create!({ value: "string value", event_timestamp: DateTime.now })
+    end
+
+    assert_raises ActiveRecord::RecordInvalid, match: "Validation failed: Value is not a number" do
+      TemperatureSample.create!({ value: true, event_timestamp: DateTime.now })
+    end
+
+    assert_raises ActiveRecord::RecordInvalid, match: "Validation failed: Value can't be blank, Value is not a number" do
+      TemperatureSample.create!({ event_timestamp: DateTime.now })
+    end
+
+    assert_raises ActiveRecord::RecordInvalid, match: "Validation failed: Value can't be blank, Value is not a number" do
+      TemperatureSample.create!({ value: nil, event_timestamp: DateTime.now })
+    end
+  end
+
+  test "should not create a sample when missing event_timestamp" do
+    assert_raises ActiveRecord::RecordInvalid, match: "Validation failed: Event timestamp can't be blank" do
+      TemperatureSample.create!({ value: 24.5 })
+    end
+  end
+
+  test "should successfully create a sample" do
+    assert_difference "TemperatureSample.count", 1 do
+      TemperatureSample.create!({ value: 24.5, event_timestamp: DateTime.now })
+    end
+  end
 end
